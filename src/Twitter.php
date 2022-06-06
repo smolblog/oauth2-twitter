493fea0c00376d965a4179f1cbccb3c27e0d8eef
Evan Hildreth <me@eph.me>
2022-06-05 20:19:10 -0400

    Update Access Token Request

--- src/Twitter.php
+++ src/Twitter.php
@@ -17,6 +17,7 @@ use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use RandomLib\Factory as RandomLibFactory;
 
@@ -29,14 +30,14 @@ class Twitter extends AbstractProvider
 {
     use BearerAuthorizationTrait;
 
-        /**
-         * In addition to state, store a PKCE verifier that will be used when
-         * getting the authorization token.
-         *
-         * @link https://www.oauth.com/oauth2-servers/pkce/authorization-code-exchange/
-         *
-         * @var string
-         */
+    /**
+     * In addition to state, store a PKCE verifier that will be used when
+     * getting the authorization token.
+     *
+     * @link https://www.oauth.com/oauth2-servers/pkce/authorization-code-exchange/
+     *
+     * @var string
+     */
     protected string $pkce_verifier;
 
     public function getPkceVerifier(): string
@@ -48,7 +49,7 @@ class Twitter extends AbstractProvider
         return $this->pkce_verifier;
     }
 
-        /**
+    /**
      * Returns the base URL for authorizing a client.
      *
      * Eg. https://oauth.service.com/authorize
@@ -72,7 +73,23 @@ class Twitter extends AbstractProvider
         return parent::getAuthorizationParameters($options);
     }
 
-        /**
+    /**
+     * Returns a prepared request for requesting an access token. Overridden
+     * to add the required headers for Twitter
+     *
+     * @param array $params Query string parameters
+     * @return RequestInterface
+     */
+    protected function getAccessTokenRequest(array $params): RequestInterface
+    {
+        $request = parent::getAccessTokenRequest($params);
+
+        $token_string = base64_encode($this->clientId . ':' . $this->clientSecret);
+
+        return $request->withHeader('Authorization', "Basic $token_string");
+    }
+
+    /**
      * Returns the base URL for requesting an access token.
      *
      * Eg. https://oauth.service.com/token
@@ -85,7 +102,7 @@ class Twitter extends AbstractProvider
         return 'https://api.twitter.com/2/oauth2/token';
     }
 
-        /**
+    /**
      * Returns the URL for requesting the resource owner's details.
      *
      * @param AccessToken $token
@@ -96,7 +113,7 @@ class Twitter extends AbstractProvider
         return 'https://api.twitter.com/2/users/me';
     }
 
-        /**
+    /**
      * Returns the default scopes used by this provider.
      *
      * This should only be the scopes that are required to request the details
@@ -113,7 +130,7 @@ class Twitter extends AbstractProvider
         ];
     }
 
-        /**
+    /**
      * Returns the string that should be used to separate scopes when building
      * the URL for requesting an access token.
      *
@@ -157,26 +174,26 @@ class Twitter extends AbstractProvider
             return new TwitterUser($response);
     }
 
-        /**
-         * Gives a URL-friendly Base64-encoded version of a string
-         *
-         * @link https://www.oauth.com/oauth2-servers/pkce/authorization-request/
-         *
-         * @param string $param String to encode
-         * @return string
-         */
+    /**
+     * Gives a URL-friendly Base64-encoded version of a string
+     *
+     * @link https://www.oauth.com/oauth2-servers/pkce/authorization-request/
+     *
+     * @param string $param String to encode
+     * @return string
+     */
     private function base64Urlencode(string $param): string
     {
         return rtrim(strtr(base64_encode($param), '+/', '-_'), '=');
     }
 
-        /**
-         * Create a PKCE verifier string.
-         *
-         * @link https://www.oauth.com/oauth2-servers/pkce/authorization-request/
-         *
-         * @return string
-         */
+    /**
+     * Create a PKCE verifier string.
+     *
+     * @link https://www.oauth.com/oauth2-servers/pkce/authorization-request/
+     *
+     * @return string
+     */
     public function generatePkceVerifier(): string
     {
         $generator = (new RandomLibFactory)->getMediumStrengthGenerator();
