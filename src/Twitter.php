d6143f4291bf926642d30c2189784cf0f65772a0
Sergio Álvarez <xergio@gmail.com>
2025-02-03 00:51:28 +0100

    php 8.4 deprecation warning (#11)

--- src/Twitter.php
+++ src/Twitter.php
@@ -235,7 +235,7 @@ class Twitter extends AbstractProvider
      * @param string $passed_verifier Verifier string to use. Defaults to $this->getPkceVerifier().
      * @return string
      */
-    public function generatePkceChallenge(string $passed_verifier = null): string
+    public function generatePkceChallenge(?string $passed_verifier = null): string
     {
         $verifier = $passed_verifier ?? $this->getPkceVerifier();
         return $this->base64Urlencode(hash('SHA256', $verifier, true));
