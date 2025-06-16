9a7337b837a3e9f8d1f2cd462eee09ef9ce19770
Rahul Kumar Sharma <39000885+jerry4rahul@users.noreply.github.com>
2025-06-17 00:32:50 +0530

    Update Twitter.php
    changed authorize and token url

--- src/Twitter.php
+++ src/Twitter.php
@@ -88,7 +88,7 @@ class Twitter extends AbstractProvider
      */
     public function getBaseAuthorizationUrl(): string
     {
-        return 'https://twitter.com/i/oauth2/authorize';
+        return 'https://x.com/i/oauth2/authorize';
     }
 
     protected function getAuthorizationParameters(array $options): array
@@ -127,7 +127,7 @@ class Twitter extends AbstractProvider
      */
     public function getBaseAccessTokenUrl(array $params): string
     {
-        return 'https://api.twitter.com/2/oauth2/token';
+        return 'https://api.x.com/2/oauth2/token';
     }
 
     /**
